4f9dd07449a23e2043537100c1f8f5780365b2a6
1582130940 <19632674+1582130940@users.noreply.github.com>
2025-01-26 02:59:03 +0800

    soong: Return release-keys
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- android/config.go
+++ android/config.go
@@ -1171,12 +1171,12 @@ func (c *config) ExtraOtaKeys(ctx PathContext, recovery bool) []SourcePath {
 func (c *config) BuildKeys() string {
 	defaultCert := String(c.productVariables.DefaultAppCertificate)
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
-		return "test-keys"
+		return "release-keys"
 	}
 	if strings.HasPrefix(defaultCert, "vendor/lineage-priv/") {
 		return "release-keys"
 	}
-	return "dev-keys"
+	return "release-keys"
 }
 
 func (c *config) ApexKeyDir(ctx ModuleContext) SourcePath {
